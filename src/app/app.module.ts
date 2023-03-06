f10de80001ae5d64a1ca979b751a7d2b84c90326
Sh1ro0o <david.pintar00@gmail.com>
2023-03-06 12:47:58 +0100

    set the layout of the webpage and added routes for users and books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { UsersComponent } from './users/users.component';
+import { BooksComponent } from './books/books.component';
+
+
+const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'books', component: BooksComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    UsersComponent,
+    BooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
